798e0150a2ca2991a1e70a85510dbf68ae881400
Nicole <nicole.bruneau@upb.edu.co>
2022-09-03 12:40:50 -0500

    minificación del .js

--- hibrida/gulpfile.js
+++ hibrida/gulpfile.js
@@ -8,8 +8,9 @@ const uglify = require('gulp-uglify-es').default;
 sass.compiler = require('dart-sass');
 
 function js(){
-  return src("./js/**/*.js")
+  return src("./js/*.js")
          .pipe(concat("main.min.js"))
+         .pipe(uglify())
          .pipe(dest('./js'));
 }
 
