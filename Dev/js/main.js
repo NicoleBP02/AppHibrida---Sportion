2024aa0647e3b6d9a5f0a0a6a094edb289ac5112
Nicole <nicole.bruneau@upb.edu.co>
2022-09-08 08:17:47 -0500

    Archivos organizados v3

---
+++ Dev/js/main.js
@@ -0,0 +1,51 @@
+var seccion_actual;
+
+localStorage.setItem('usuario', 'admin')
+localStorage.setItem('password', 'something123')
+
+function ocultar()
+{
+  let secciones = document.getElementsByClassName("seccion"); 
+  for(var i=0;i<secciones.length;i++){
+    secciones[i].classList.add("ocultar");
+    secciones[i].classList.remove("animate__bounceInRight");
+  }
+
+}
+
+function cambiarSeccion(id)
+{
+  let seccion = document.getElementById("seccion_"+id);
+  ocultar();
+  seccion.classList.remove("ocultar");
+  seccion.classList.add("animate__bounceInRight");
+}
+
+function login(){
+  let usuario = document.getElementById('usuario');
+  let password = document.getElementById('password');
+
+  if (usuario.value == localStorage.getItem('usuario') && password.value == localStorage.getItem('password')){
+    console.log("Successful login")
+    usuario.value = "";
+    password.value = "";
+    cambiarSeccion(1);
+  }
+  else{
+    console.log("Error on login - Wrong username/password")
+    usuario.style.background = "#ff000022"
+    password.style.background = "#ff000022"
+  } 
+}
+
+function reservar(){
+  console.log("Launching reservar function...");
+  let seleccion = document.querySelector('input[name="reservar"]:checked');
+  console.log("You selected " + seleccion.value);
+  localStorage.setItem('reservas', seleccion.value);
+  let listareservas = document.getElementById('listareservas');
+  let newreserva = document.createElement('p')
+  newreserva.innerHTML = localStorage.getItem('reservas')
+  listareservas.appendChild(newreserva)
+  cambiarSeccion(6);
+}
---
+++ www/README.md
@@ -0,0 +1,12 @@
+# AppHibrida
+
+Repositorio de la aplicación híbrida Sportion
+
+## Instrucciones de uso
+
+Primero debe descargar las dependencias de la aplicación con npm install
+
+# IMPORTANTE
+
+usuario: admin
+constraseña: something123
